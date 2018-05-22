eb44fe518d3026b068a3f4080d6f53b94a1aac7d
YANGLiiN <yanglin5@sf-express.com>
2018-05-22 17:07:43 +0800

    [option] make the HashedWheelTimer ThreadFactory daemon

--- src/main/java/com/alipay/remoting/TimerHolder.java
+++ src/main/java/com/alipay/remoting/TimerHolder.java
@@ -33,8 +33,8 @@ public class TimerHolder {
 
     private static class DefaultInstance {
         static final Timer INSTANCE = new HashedWheelTimer(new NamedThreadFactory(
-                                        "DefaultTimer" + defaultTickDuration), defaultTickDuration,
-                                        TimeUnit.MILLISECONDS);
+                                        "DefaultTimer" + defaultTickDuration, true),
+                                        defaultTickDuration, TimeUnit.MILLISECONDS);
     }
 
     private TimerHolder() {
