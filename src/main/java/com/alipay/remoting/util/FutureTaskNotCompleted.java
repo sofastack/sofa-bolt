de44e2a38fed030b6be3f83649efbeb9c7914d2f
linxin <marklin.hz@gmail.com>
2018-10-18 03:09:26 +0800

    Make getAfterRun un-block avoid deadlock. (#106)

---
+++ src/main/java/com/alipay/remoting/util/FutureTaskNotCompleted.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.remoting.util;
+
+/**
+ * Exception to represent the run method of a future task has not been finished.
+ *
+ * @author chengyi (mark.lx@antfin.com) 2018-10-18 02:06
+ */
+public class FutureTaskNotCompleted extends Exception {
+
+    private static final long serialVersionUID = -3635466558774380138L;
+
+    public FutureTaskNotCompleted() {
+
+    }
+
+    public FutureTaskNotCompleted(String message) {
+        super(message);
+    }
+
+    public FutureTaskNotCompleted(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
