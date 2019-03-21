151f9e29372c5b8ff3be39bf71e6e9c4e2aabbd0
chengyi <mark.lx@antfin.com>
2019-03-21 17:01:56 +0800

    add LifeCycle interface to RemotingServer

--- src/main/java/com/alipay/remoting/AbstractRemotingServer.java
+++ src/main/java/com/alipay/remoting/AbstractRemotingServer.java
@@ -17,7 +17,6 @@
 package com.alipay.remoting;
 
 import java.net.InetSocketAddress;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.alipay.remoting.config.BoltOption;
 import com.alipay.remoting.config.BoltOptions;
@@ -42,9 +41,8 @@ import com.alipay.remoting.log.BoltLoggerFactory;
 public abstract class AbstractRemotingServer extends AbstractLifeCycle implements RemotingServer,
                                                                       ConfigurableInstance {
 
-    private static final Logger   logger  = BoltLoggerFactory.getLogger("CommonDefault");
+    private static final Logger   logger = BoltLoggerFactory.getLogger("CommonDefault");
 
-    private AtomicBoolean         started = new AtomicBoolean(false);
     private String                ip;
     private int                   port;
 
@@ -68,41 +66,50 @@ public abstract class AbstractRemotingServer extends AbstractLifeCycle implement
     }
 
     @Override
+    @Deprecated
     public void init() {
         // Do not call this method, it will be removed in the next version
     }
 
     @Override
+    @Deprecated
     public boolean start() {
-        if (started.compareAndSet(false, true)) {
-            try {
-                doInit();
+        startup();
+        return true;
+    }
 
-                logger.warn("Prepare to start server on port {} ", port);
-                if (doStart()) {
-                    logger.warn("Server started on port {}", port);
-                    return true;
-                } else {
-                    logger.warn("Failed starting server on port {}", port);
-                    return false;
-                }
-            } catch (Throwable t) {
-                this.stop();// do stop to ensure close resources created during doInit()
-                throw new IllegalStateException("ERROR: Failed to start the Server!", t);
+    @Override
+    @Deprecated
+    public boolean stop() {
+        shutdown();
+        return true;
+    }
+
+    @Override
+    public void startup() throws LifeCycleException {
+        super.startup();
+
+        try {
+            doInit();
+
+            logger.warn("Prepare to start server on port {} ", port);
+            if (doStart()) {
+                logger.warn("Server started on port {}", port);
+            } else {
+                logger.warn("Failed starting server on port {}", port);
+                throw new LifeCycleException("Failed starting server on port: " + port);
             }
-        } else {
-            String errMsg = "ERROR: The server has already started!";
-            logger.error(errMsg);
-            throw new IllegalStateException(errMsg);
+        } catch (Throwable t) {
+            this.shutdown();// do stop to ensure close resources created during doInit()
+            throw new IllegalStateException("ERROR: Failed to start the Server!", t);
         }
     }
 
     @Override
-    public boolean stop() {
-        if (started.compareAndSet(true, false)) {
-            return this.doStop();
-        } else {
-            throw new IllegalStateException("ERROR: The server has already stopped!");
+    public void shutdown() throws LifeCycleException {
+        super.shutdown();
+        if (!doStop()) {
+            throw new LifeCycleException("doStop fail");
         }
     }
 
