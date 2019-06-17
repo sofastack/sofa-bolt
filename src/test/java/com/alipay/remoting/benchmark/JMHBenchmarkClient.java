4910f75b0bf45d9404e64f3cb77de557d768bf16
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2019-06-17 11:44:08 +0800

    (feat) add jmh benchmark test for flush consolidation

--- src/test/java/com/alipay/remoting/benchmark/JMHBenchmarkClient.java
+++ src/test/java/com/alipay/remoting/benchmark/JMHBenchmarkClient.java
@@ -45,14 +45,58 @@ public class JMHBenchmarkClient {
 
     /**
      * CODEC_FLUSH_CONSOLIDATION=turnOn
-     * Benchmark                     Mode  Cnt   Score    Error   Units
-     * JMHBenchmarkClient.send1024  thrpt    3  51.310 ± 11.585  ops/ms
-     * JMHBenchmarkClient.send128   thrpt    3  68.493 ± 20.078  ops/ms
+     * Benchmark                                       Mode      Cnt   Score    Error   Units
+     * JMHBenchmarkClient.send1024                    thrpt        3  46.887 ± 22.766  ops/ms
+     * JMHBenchmarkClient.send128                     thrpt        3  66.376 ± 28.429  ops/ms
+     * JMHBenchmarkClient.send1024                     avgt        3   0.674 ±  0.986   ms/op
+     * JMHBenchmarkClient.send128                      avgt        3   0.479 ±  0.239   ms/op
+     * JMHBenchmarkClient.send1024                   sample  1525751   0.629 ±  0.001   ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.00    sample            0.150            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.50    sample            0.602            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.90    sample            0.775            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.95    sample            0.854            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.99    sample            1.614            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.999   sample            2.249            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.9999  sample            4.280            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p1.00    sample            6.431            ms/op
+     * JMHBenchmarkClient.send128                    sample  1913189   0.501 ±  0.001   ms/op
+     * JMHBenchmarkClient.send128:send128·p0.00      sample            0.150            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.50      sample            0.481            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.90      sample            0.618            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.95      sample            0.688            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.99      sample            1.190            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.999     sample            2.097            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.9999    sample            4.391            ms/op
+     * JMHBenchmarkClient.send128:send128·p1.00      sample            7.053            ms/op
+     * JMHBenchmarkClient.send1024                       ss        3   5.179 ± 18.142   ms/op
+     * JMHBenchmarkClient.send128                        ss        3   3.788 ± 10.388   ms/op
      *
      * CODEC_FLUSH_CONSOLIDATION=turnOff
-     * Benchmark                     Mode  Cnt   Score    Error   Units
-     * JMHBenchmarkClient.send1024  thrpt    3  37.769 ± 31.279  ops/ms
-     * JMHBenchmarkClient.send128   thrpt    3  44.074 ± 32.570  ops/ms
+     * Benchmark                                       Mode      Cnt   Score    Error   Units
+     * JMHBenchmarkClient.send1024                    thrpt        3  35.946 ± 10.332  ops/ms
+     * JMHBenchmarkClient.send128                     thrpt        3  41.364 ± 36.002  ops/ms
+     * JMHBenchmarkClient.send1024                     avgt        3   0.846 ±  0.272   ms/op
+     * JMHBenchmarkClient.send128                      avgt        3   0.731 ±  0.306   ms/op
+     * JMHBenchmarkClient.send1024                   sample  1003714   0.956 ±  0.001   ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.00    sample            0.183            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.50    sample            0.886            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.90    sample            1.294            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.95    sample            1.495            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.99    sample            2.241            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.999   sample            3.977            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p0.9999  sample            8.448            ms/op
+     * JMHBenchmarkClient.send1024:send1024·p1.00    sample           21.660            ms/op
+     * JMHBenchmarkClient.send128                    sample  1346879   0.712 ±  0.001   ms/op
+     * JMHBenchmarkClient.send128:send128·p0.00      sample            0.124            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.50      sample            0.684            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.90      sample            0.927            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.95      sample            1.016            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.99      sample            1.571            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.999     sample            2.322            ms/op
+     * JMHBenchmarkClient.send128:send128·p0.9999    sample            4.375            ms/op
+     * JMHBenchmarkClient.send128:send128·p1.00      sample            5.865            ms/op
+     * JMHBenchmarkClient.send1024                       ss        3   5.932 ± 13.160   ms/op
+     * JMHBenchmarkClient.send128                        ss        3   5.028 ± 10.273   ms/op
      */
 
     public static final int     CONCURRENCY = 32;
@@ -82,14 +126,14 @@ public class JMHBenchmarkClient {
     }
 
     @Benchmark
-    @BenchmarkMode(Mode.Throughput)
+    @BenchmarkMode(Mode.All)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     public void send128() throws RemotingException, InterruptedException {
         client.invokeSync("127.0.0.1:18090", new Request<byte[]>(BYTES_128), 5000);
     }
 
     @Benchmark
-    @BenchmarkMode(Mode.Throughput)
+    @BenchmarkMode(Mode.All)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     public void send1024() throws RemotingException, InterruptedException {
         client.invokeSync("127.0.0.1:18090", new Request<byte[]>(BYTES_1024), 5000);
