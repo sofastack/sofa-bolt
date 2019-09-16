bdccc3b92785a9326bd77721c4cb0e906f0316c7
muyun <muyun.cyt@antfin.com>
2019-09-16 14:48:59 +0800

    fix spelling mistake

--- src/main/java/com/alipay/remoting/rpc/protocol/AsyncMultiInterestUserProcessor.java
+++ src/main/java/com/alipay/remoting/rpc/protocol/AsyncMultiInterestUserProcessor.java
@@ -22,35 +22,32 @@ import com.alipay.remoting.AsyncContext;
 import com.alipay.remoting.BizContext;
 
 /**
- * Extends this to process user defined request in SYNC way.<br>
- * If you want process reqeuest in ASYNC way, please extends {@link AsynMultiInterestUserProcessor}.
- *
+ * Extends this to process user defined request in ASYNC way.<br>
+ * If you want process request in SYNC way, please extends {@link SyncMultiInterestUserProcessor}.
  * @author muyun.cyt (muyun.cyt@antfin.com)  2018/7/5   11:19 AM
  */
-public abstract class SyncMutiInterestUserProcessor<T> extends
-                                                       AbstractMultiInterestUserProcessor<T> {
-
-    /**
-     * @see com.alipay.remoting.rpc.protocol.UserProcessor#handleRequest(com.alipay.remoting.BizContext, java.lang.Object)
-     */
-    @Override
-    public abstract Object handleRequest(BizContext bizCtx, T request) throws Exception;
-
+public abstract class AsyncMultiInterestUserProcessor<T> extends
+                                                        AbstractMultiInterestUserProcessor<T> {
     /**
      * unsupported here!
      *
+     * @see com.alipay.remoting.rpc.protocol.UserProcessor#handleRequest(com.alipay.remoting.BizContext, java.lang.Object)
+     */
+    @Override
+    public Object handleRequest(BizContext bizCtx, T request) throws Exception {
+        throw new UnsupportedOperationException(
+            "SYNC handle request is unsupported in AsyncMultiInterestUserProcessor!");
+    }
+
+    /**
      * @see com.alipay.remoting.rpc.protocol.UserProcessor#handleRequest(com.alipay.remoting.BizContext, com.alipay.remoting.AsyncContext, java.lang.Object)
      */
     @Override
-    public void handleRequest(BizContext bizCtx, AsyncContext asyncCtx, T request) {
-        throw new UnsupportedOperationException(
-            "ASYNC handle request is unsupported in SyncMutiInterestUserProcessor!");
-    }
+    public abstract void handleRequest(BizContext bizCtx, AsyncContext asyncCtx, T request);
 
     /**
      * @see com.alipay.remoting.rpc.protocol.MultiInterestUserProcessor#multiInterest()
      */
     @Override
     public abstract List<String> multiInterest();
-
 }
