9645249bc72b4f7624ab4bfbb15b3d815c81cefa
lollapalooza1989 <yuren@wacai.com>
2019-10-10 14:48:05 +0800

    Revert "fix  AsynMultiInterestUserProcessor dispatch to sync handleRequest  method problem. gh-157"
    This reverts commit 2784f9e13780a1fb19a56f63781db250808cce32.

--- src/main/java/com/alipay/remoting/rpc/protocol/RpcRequestProcessor.java
+++ src/main/java/com/alipay/remoting/rpc/protocol/RpcRequestProcessor.java
@@ -219,8 +219,7 @@ public class RpcRequestProcessor extends AbstractRemotingProcessor<RpcRequestCom
         final byte type = cmd.getType();
         // processor here must not be null, for it have been checked before
         UserProcessor processor = ctx.getUserProcessor(cmd.getRequestClass());
-        if (processor instanceof AsyncUserProcessor
-            || processor instanceof AsynMultiInterestUserProcessor) {
+        if (processor instanceof AsyncUserProcessor) {
             try {
                 processor.handleRequest(processor.preHandleRequest(ctx, cmd.getRequestObject()),
                     new RpcAsyncContext(ctx, cmd, this), cmd.getRequestObject());
