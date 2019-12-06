248f97fdfb12e222baf9659f75e7454ce3b26f3e
cytnju <muyun.cyt@antfin.com>
2019-12-06 16:35:41 +0800

    fix spell mistake and some test codes (#194)

--- src/main/java/com/alipay/remoting/rpc/protocol/AsyncUserProcessor.java
+++ src/main/java/com/alipay/remoting/rpc/protocol/AsyncUserProcessor.java
@@ -21,15 +21,15 @@ import com.alipay.remoting.BizContext;
 
 /**
  * Extends this to process user defined request in ASYNC way.<br>
- * If you want process reqeuest in SYNC way, please extends {@link SyncUserProcessor}.
- * 
+ * If you want process request in SYNC way, please extends {@link SyncUserProcessor}.
+ *
  * @author xiaomin.cxm
  * @version $Id: AsyncUserProcessor.java, v 0.1 May 16, 2016 8:18:03 PM xiaomin.cxm Exp $
  */
 public abstract class AsyncUserProcessor<T> extends AbstractUserProcessor<T> {
     /**
      * unsupported here!
-     * 
+     *
      * @see com.alipay.remoting.rpc.protocol.UserProcessor#handleRequest(com.alipay.remoting.BizContext, java.lang.Object)
      */
     @Override
