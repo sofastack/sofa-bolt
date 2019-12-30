eab252bb226dcaf46c99220232eea8ed6de80cfe
mushuwei <lovewsic@gmail.com>
2019-12-30 12:07:20 +0800

    fix spelling mistakes

--- src/main/java/com/alipay/remoting/NamedThreadFactory.java
+++ src/main/java/com/alipay/remoting/NamedThreadFactory.java
@@ -41,10 +41,10 @@ public class NamedThreadFactory implements ThreadFactory {
         this(name, false);
     }
 
-    public NamedThreadFactory(String preffix, boolean daemon) {
+    public NamedThreadFactory(String prefix, boolean daemon) {
         SecurityManager s = System.getSecurityManager();
         group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-        namePrefix = preffix + "-" + poolNumber.getAndIncrement() + "-thread-";
+        namePrefix = prefix + "-" + poolNumber.getAndIncrement() + "-thread-";
         isDaemon = daemon;
     }
 
