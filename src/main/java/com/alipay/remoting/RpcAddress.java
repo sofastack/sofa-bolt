1d863158124cc8f96ceca3c58f1cdbdf5735bec4
je <2285262007@qq.com>
2021-12-13 20:11:35 +0800

    fix #263 add IPv6 Support

--- src/main/java/com/alipay/remoting/RpcAddress.java
+++ src/main/java/com/alipay/remoting/RpcAddress.java
@@ -31,10 +31,8 @@ public class RpcAddress {
     private static final Pattern IPV6_REGEX_NORMAL = Pattern.compile(
             "\\[((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:)))](%.+)?");
 
-
     private final String url;
-    private String ip;
-
+    private       String ip;
 
     /**
      * Constructor, accepts url parameters, resolve the ip and protocol version of the Rpc address
@@ -43,8 +41,8 @@ public class RpcAddress {
      */
     private RpcAddress(String url) {
         if (StringUtils.isBlank(url)) {
-            throw new IllegalArgumentException("Illegal format address string [" + url
-                    + "], should not be blank! ");
+            throw new IllegalArgumentException(
+                    "Illegal format address string [" + url + "], should not be blank! ");
         }
         this.url = url;
         this.parse();
@@ -67,7 +65,7 @@ public class RpcAddress {
     private void parse() {
         if (!tryParseV4() && !tryParseV6()) {
             throw new IllegalArgumentException("Illegal format address string [" + url
-                    + "], It should conform to the ipv4 protocol or ipv6 protocol address format! ");
+                                               + "], It should conform to the ipv4 protocol or ipv6 protocol address format! ");
         }
     }
 
@@ -104,10 +102,14 @@ public class RpcAddress {
             return false;
         }
         if (url.startsWith(ip)) {
+            if (ip.length() == url.length()) {
+                throw new IllegalArgumentException("Illegal format address string [" + url
+                                                   + "], must have one COLON[:] after ip string! ");
+            }
             final char firstCharAfterIp = url.charAt(ip.length());
             if (firstCharAfterIp != COLON) {
                 throw new IllegalArgumentException("Illegal format address string [" + url
-                        + "], must have one COLON[:] after ip string! ");
+                                                   + "], must have one COLON[:] after ip string! ");
             }
             this.ip = ip;
 
@@ -115,7 +117,6 @@ public class RpcAddress {
         }
         return false;
     }
-
 
     public String getIp() {
         return ip;
