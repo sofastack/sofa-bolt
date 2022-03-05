c4764fc6ead54d03e62e1eb28dba1ccb88cf67e4
chuailiwu <chuailiwu@163.com>
2022-03-05 18:43:21 +0800

    add ut

--- src/test/java/com/alipay/remoting/simpledemo/QuickStartServerAndClientWithIpv6.java
+++ src/test/java/com/alipay/remoting/simpledemo/QuickStartServerAndClientWithIpv6.java
@@ -20,16 +20,16 @@ import com.alipay.remoting.exception.RemotingException;
 import com.alipay.remoting.rpc.RpcClient;
 import com.alipay.remoting.rpc.RpcServer;
 
-public class QuickStartServerAndClient {
+public class QuickStartServerAndClientWithIpv6 {
     public static void main(String[] args) throws RemotingException, InterruptedException {
-        RpcServer rpcServer = new RpcServer( 9876);
+        RpcServer rpcServer = new RpcServer("0:0:0:0:0:0:0:1", 9875);
         rpcServer.registerUserProcessor(new SimpleUserProcessor());
         rpcServer.startup();
 
         RpcClient rpcClient = new RpcClient();
         rpcClient.startup();
         for (int i = 0; i < 10; i++) {
-            SimpleResponse response = (SimpleResponse) rpcClient.invokeSync("127.0.0.1:9876",
+            SimpleResponse response = (SimpleResponse) rpcClient.invokeSync("0:0:0:0:0:0:0:1:9875",
                 new SimpleRequest(i), 1000);
             System.out.println("i=" + i + " res=" + response.getRes());
             Thread.sleep(1000);
