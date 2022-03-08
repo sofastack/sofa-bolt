304449ebe6b9c07ef011a452793ec2b8e0785f70
chuailiwu <chuailiwu@163.com>
2022-03-08 16:14:33 +0800

    support ipv6 address parse (#282)
    * add simple test
    
    * modify log dependencies and a simple demo
    
    * ipv6 parse
    
    * add License
    
    * fix int convert to short
    
    * add ut
    
    * fix typo
    
    * rm unnecessary test
    
    * add ipv6 rpc ut
    
    Co-authored-by: 三樵 <liwu.clw@antgroup.com>

--- src/test/java/com/alipay/remoting/rpc/RpcCommandTest.java
+++ src/test/java/com/alipay/remoting/rpc/RpcCommandTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.remoting.rpc;
 
 import com.alipay.remoting.rpc.protocol.RpcRequestCommand;
@@ -5,7 +21,6 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
 
 public class RpcCommandTest {
     @Rule
@@ -20,7 +35,7 @@ public class RpcCommandTest {
     }
 
     @Test
-    public void setClazz_exceed_maximum(){
+    public void setClazz_exceed_maximum() {
         RpcCommand rpcCommand = new RpcRequestCommand();
         byte[] clazz = new byte[Short.MAX_VALUE + 1];
         thrown.expect(RuntimeException.class);
@@ -37,7 +52,7 @@ public class RpcCommandTest {
     }
 
     @Test
-    public void setHeader_exceed_maximum(){
+    public void setHeader_exceed_maximum() {
         RpcCommand rpcCommand = new RpcRequestCommand();
         byte[] header = new byte[Short.MAX_VALUE + 1];
         thrown.expect(RuntimeException.class);
