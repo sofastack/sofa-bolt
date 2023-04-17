396d4bf0fc5cd534545d6707d1c61fa4914537fb
chuailiwu <chuailiwu@163.com>
2023-04-17 12:40:59 +0800

    Fix addConnectionEventProcessor add ut

---
+++ src/test/java/com/alipay/remoting/ConnectionEventListenerTest.java
@@ -0,0 +1,48 @@
+package com.alipay.remoting;
+
+import com.alipay.remoting.rpc.RpcClient;
+import com.alipay.remoting.serialization.HessianSerializerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class ConnectionEventListenerTest {
+
+    @Test
+    public void addConnectionEventProcessorConcurrentTest() throws InterruptedException {
+        int concurrentNum = 100;
+        CountDownLatch countDownLatch = new CountDownLatch(concurrentNum);
+        RpcClient rpcClient = new RpcClient();
+        for (int i = 0; i < concurrentNum; ++i) {
+            MyThread thread = new MyThread(countDownLatch, rpcClient);
+            new Thread(thread).start();
+        }
+        Assert.assertTrue(countDownLatch.await(2, TimeUnit.SECONDS));
+    }
+
+    static class MyThread implements Runnable {
+        CountDownLatch countDownLatch;
+        RpcClient rpcClient;
+
+        public MyThread(CountDownLatch countDownLatch, RpcClient rpcClient) {
+            this.countDownLatch = countDownLatch;
+            this.rpcClient = rpcClient;
+        }
+
+        @Override
+        public void run() {
+            try {
+                rpcClient.addConnectionEventProcessor(ConnectionEventType.CONNECT, (remoteAddress, connection) -> {});
+            } catch (Exception e) {
+                fail();
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
+    }
+}
