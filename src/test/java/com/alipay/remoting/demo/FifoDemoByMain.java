32899a98956865d04bc9a9c57bcb89ed94cf4d36
chuailiwu <chuailiwu@163.com>
2023-04-28 10:01:39 +0800

    add demo for fifo

--- src/test/java/com/alipay/remoting/demo/FifoDemoByMain.java
+++ src/test/java/com/alipay/remoting/demo/FifoDemoByMain.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.remoting.demo;
 
 import com.alipay.remoting.Connection;
@@ -23,7 +39,7 @@ public class FifoDemoByMain {
         server.registerUserProcessor(serverUserProcessor);
 
         // 3. key point: close the ability to dispatch msg list to another thead pool
-        System.setProperty(RpcConfigs.DISPATCH_MSG_LIST_IN_DEFAULT_EXECUTOR,"false");
+        System.setProperty(RpcConfigs.DISPATCH_MSG_LIST_IN_DEFAULT_EXECUTOR, "false");
 
         // 4. server start
         server.start();
